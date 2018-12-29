0fe9ab33d3074bb1f72e7b7836dc66e69ddbca75
IYU <issacyu2013@gmail.com>
2018-12-29 15:37:24 -0500

    Add a ResponsiveContainer to make the BarChart become responsive.

--- src/components/Charts/BarChart.js
+++ src/components/Charts/BarChart.js
@@ -7,7 +7,8 @@ import {
     YAxis, 
     CartesianGrid, 
     Tooltip, 
-    Legend
+    Legend,
+    ResponsiveContainer
 } from 'recharts';
 
 const data = [
@@ -22,16 +23,18 @@ const data = [
 
 const BarCharts = () => {
     return (
-        <BarChart width={600} height={300} data={data}
-            margin={{top: 5, right: 30, left: 20, bottom: 5}}>
-        <CartesianGrid strokeDasharray="3 3"/>
-        <XAxis dataKey="name"/>
-        <YAxis/>
-        <Tooltip/>
-        <Legend />
-        <Bar dataKey="pv" fill="#8884d8" />
-        <Bar dataKey="uv" fill="#82ca9d" />
-        </BarChart>
+        <ResponsiveContainer width="100%" height={1000}>
+            <BarChart width={600} height={300} data={data}
+                margin={{top: 5, right: 30, left: 20, bottom: 5}}>
+                <CartesianGrid strokeDasharray="3 3"/>
+                <XAxis dataKey="name"/>
+                <YAxis/>
+                <Tooltip/>
+                <Legend />
+                <Bar dataKey="pv" fill="#8884d8" />
+                <Bar dataKey="uv" fill="#82ca9d" />
+            </BarChart>
+        </ResponsiveContainer>
     )
 };
 
