a5d6dd6ee8dd42c9ae296765cb9bcdc30996a5c4
IYU <issacyu2013@gmail.com>
2019-01-05 13:30:46 -0500

    Move all the components from Charts to Overview.

--- src/containers/Charts/Charts.js
+++ src/containers/Charts/Charts.js
@@ -1,37 +1,10 @@
 import React from 'react';
 
-import BarChart from '../../components/Charts/BarChart';
-import LineChart from '../../components/Charts/LineChart';
-import PieChart from '../../components/Charts/PieChart';
-import Card from '../../components/Card/Card';
-import { Grid, Row, Col } from 'react-bootstrap';
-
 const Charts = () => {
     return (
-        <>
-            <Grid fluid={true}>
-                <Row className="show-grid text-center">
-                    <Card heading='Sales'/>
-                    <Card heading='Revenue'/>
-                    <Card heading='Orders'/>
-                    <Card heading='Fees'/>
-                </Row>
-                <Row className="show-grid text-center">
-                    <Row md={12} lg={12}>
-                        <LineChart />
-                    </Row>
-                    <Row>
-                        <Col md={8} lg={8}>
-                            <BarChart />
-                        </Col>
-                        <Col md={4} lg={4}>
-                            <PieChart />
-                        </Col>
-                    </Row>
-                </Row>
-            </Grid>
-            
-        </>
+        <div>
+            <h1>This is Charts!!!!</h1>
+        </div>
     )
 }
 
--- src/containers/Overview/Overview.js
+++ src/containers/Overview/Overview.js
@@ -1,12 +1,40 @@
 import React, { Component } from 'react';
 
+import Card from '../../components/Card/Card';
+import { Grid, Row, Col } from 'react-bootstrap';
+
+import BarChart from '../../components/Charts/BarChart';
+import LineChart from '../../components/Charts/LineChart';
+import PieChart from '../../components/Charts/PieChart';
+
 class Overview extends Component {
 
     render(){
         return(
-            <div>
-                <h1>This is an Overview!!!</h1>
-            </div>
+            <>
+                <Grid fluid={true}>
+                    <Row className="show-grid text-center">
+                        <Card heading='Sales'/>
+                        <Card heading='Revenue'/>
+                        <Card heading='Orders'/>
+                        <Card heading='Fees'/>
+                    </Row>
+                    <Row className="show-grid text-center">
+                        <Row md={12} lg={12}>
+                            <LineChart />
+                        </Row>
+                        <Row>
+                            <Col md={8} lg={8}>
+                                <BarChart />
+                            </Col>
+                            <Col md={4} lg={4}>
+                                <PieChart />
+                            </Col>
+                        </Row>
+                    </Row>
+                </Grid>
+                
+            </>
         )
     }
 }
