25baac3ab8cb64c8a723b2994b21ddf15d13b5f9
IYU <issacyu2013@gmail.com>
2019-03-03 15:01:17 -0500

    Add a get request that retrive all sales data from sql server.

---
+++ Web API/DashboardWebApi/DashboardWebApi/Controllers/SalesController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using AutoMapper;
+
+using DashboardWebApi.Services;
+using DashboardWebApi.Entities;
+using DashboardWebApi.ViewModels;
+
+namespace DashboardWebApi.Controllers
+{
+    [Route("api/sales")]
+    public class SalesController : Controller
+    {
+        public SalesController(ISalesRepostory salesRepostory)
+        {
+            _salesRepostory = salesRepostory;
+        }
+
+        [HttpGet()]
+        public IActionResult GetSales()
+        {
+            var salesFromRepo = _salesRepostory.GetSales();
+
+            IEnumerable<SalesViewModel> sales = Mapper.Map<IEnumerable<SalesViewModel>>(salesFromRepo);
+            return Ok(sales);
+        }
+
+        private ISalesRepostory _salesRepostory;
+    }
+}
--- Web API/DashboardWebApi/DashboardWebApi/Services/SalesRepository.cs
+++ Web API/DashboardWebApi/DashboardWebApi/Services/SalesRepository.cs
@@ -2,15 +2,24 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+
 using DashboardWebApi.Entities;
 
 namespace DashboardWebApi.Services
 {
     public class SalesRepository : ISalesRepostory
     {
+        public SalesRepository(DashboardContext context)
+        {
+            _context = context;
+        }
+
         public IEnumerable<Sales> GetSales()
         {
-            throw new NotImplementedException();
+            return Sales;
         }
+
+        private DashboardContext _context;
+        private IEnumerable<Sales> Sales => _context.Sales;
     }
 }
