7897d325874ca43a54d3e14793f7ec312ce88b97
IYU <issacyu2013@gmail.com>
2019-03-10 17:42:25 -0400

    Add get request for inventory controller.

--- Web API/DashboardWebApi/DashboardWebApi/Controllers/InventoriesController.cs
+++ Web API/DashboardWebApi/DashboardWebApi/Controllers/InventoriesController.cs
@@ -3,10 +3,32 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using AutoMapper;
+
+using DashboardWebApi.Services;
+using DashboardWebApi.Services.Interfaces;
+using DashboardWebApi.Entities;
+using DashboardWebApi.ViewModels;
 
 namespace DashboardWebApi.Controllers
 {
+    [Route("api/inventories")]
     public class InventoriesController : Controller
     {
+        public InventoriesController(IInventoryRepository inventoryRepository)
+        {
+            _inventoryRepository = inventoryRepository;
+        }
+
+        [HttpGet()]
+        public IActionResult GetInventories()
+        {
+            IEnumerable<Inventory> inventoryFromRepo = _inventoryRepository.GetInventories();
+
+            IEnumerable<InventoryViewModel> inventories = Mapper.Map<IEnumerable<InventoryViewModel>>(inventoryFromRepo);
+            return Ok(inventories);
+        }
+
+        private IInventoryRepository _inventoryRepository;
     }
 }
