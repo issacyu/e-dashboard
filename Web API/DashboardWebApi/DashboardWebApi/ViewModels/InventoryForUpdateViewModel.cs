5c796505659f0408bceea5d99a4994d0c25032d3
IYU <issacyu2013@gmail.com>
2019-03-11 18:46:45 -0400

    Create InventoryForUpdateViewModel

---
+++ Web API/DashboardWebApi/DashboardWebApi/ViewModels/InventoryForUpdateViewModel.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DashboardWebApi.ViewModels
+{
+    public class InventoryForUpdateViewModel
+    {
+        public Guid Id { get; set; }
+
+        public string Product { get; set; }
+
+        public int Quantity { get; set; }
+
+        public decimal Price { get; set; }
+
+        public string Category { get; set; }
+
+        public string Model { get; set; }
+
+        public string Type { get; set; }
+
+        public string Color { get; set; }
+
+        public string Condition { get; set; }
+
+        public string Manufacturer { get; set; }
+
+        public decimal Fee { get; set; }
+
+        public decimal Cost { get; set; }
+
+        public decimal TotalCost { get; set; }
+    }
+}
