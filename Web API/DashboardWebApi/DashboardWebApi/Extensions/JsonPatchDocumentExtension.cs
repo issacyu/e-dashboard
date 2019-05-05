080389e9c8768b372797a1a1cde82e57d70b3fcf
IYU <issacyu2013@gmail.com>
2019-05-05 12:22:44 -0400

    Create JsonPatchDocumentExtension.cs
    Create an extension class and methods for JsonPatchDocument to modify the JsonPatch path.

---
+++ Web API/DashboardWebApi/DashboardWebApi/Extensions/JsonPatchDocumentExtension.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using DashboardWebApi.DTOs;
+using Microsoft.AspNetCore.JsonPatch;
+using Microsoft.AspNetCore.JsonPatch.Operations;
+
+namespace DashboardWebApi.Extensions
+{
+    public static class JsonPatchDocumentExtension
+    {
+        /// <summary>
+        /// Handle the edge case when adding new row to any data grid.
+        /// When adding new row to a grid, the path in patchDoc should
+        /// be "/-"(it means adding new array item to the end of array).
+        /// </summary>
+        /// <param name="patchDoc">The json patch document.</param>
+        public static async Task ModifyPatchPath(this JsonPatchDocument<IEnumerable<SaleForUpdateDto>> patchDoc, int collectionSize)
+        {
+            foreach (Operation operation in patchDoc.Operations)
+            {
+                if (operation.OperationType.ToString() == "Add" && int.TryParse(operation.path.Split("/")[1], out var index))
+                {
+                    if (index >= collectionSize)
+                    {
+                        operation.path = "/-";
+                    }
+                }
+            }
+        }
+
+        /// <summary>
+        /// Handle the edge case when adding new row to any data grid.
+        /// When adding new row to a grid, the path in patchDoc should
+        /// be "/-"(it means adding new array item to the end of array).
+        /// </summary>
+        /// <param name="patchDoc">The json patch document.</param>
+        public static async Task ModifyPatchPath(this JsonPatchDocument<IEnumerable<InventoryForUpdateDto>> patchDoc, int collectionSize)
+        {
+            foreach (Operation operation in patchDoc.Operations)
+            {
+                if (operation.OperationType.ToString() == "Add" && int.TryParse(operation.path.Split("/")[1], out var index))
+                {
+                    if (index >= collectionSize)
+                    {
+                        operation.path = "/-";
+                    }
+                }
+            }
+        }
+    }
+}
